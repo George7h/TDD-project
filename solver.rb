ba503c85f84290e584a773133830ec8f2af64db1
George <georgie.george7@gmail.com>
2023-12-14 16:06:36 +0400

    Add reverse method to Solver class

--- solver.rb
+++ solver.rb
@@ -4,4 +4,11 @@ class Solver
 
     (1..number).reduce(1, :*)
   end
+
+  def reverse(word)
+    word.reverse
+  end
 end
+console = Solver.new
+puts console.factorial(5)
+puts console.reverse('hello')
