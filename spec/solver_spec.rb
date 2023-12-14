5b37012e27dd3a6f3fddc730bdf1d8c6fb47ab03
Karan Joshi <Karan190213@gmail.com>
2023-12-14 16:08:18 +0000

    Update solver_spec.rb

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,20 +1,16 @@
 # spec/solver_spec.rb
 require './solver'
-
 describe Solver do
   describe '#factorial' do
     it 'returns 1 for 0' do
       expect(subject.factorial(0)).to eq(1)
     end
-
     it 'returns 1 for 1' do
       expect(subject.factorial(1)).to eq(1)
     end
-
     it 'returns the correct factorial for a positive integer' do
       expect(subject.factorial(5)).to eq(120)
     end
-
     it 'raises an ArgumentError for a negative integer' do
       expect { subject.factorial(-1) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
     end
@@ -22,7 +18,7 @@ describe Solver do
   describe '#reverse' do
     it 'returns the reversed word' do
       expect(subject.reverse('hello')).to eq('olleh')
-    end    
+    end
     it 'returns the reversed word for any input' do
       expect(subject.reverse('world')).to eq('dlrow')
     end
@@ -42,25 +38,3 @@ describe Solver do
     end
   end
 end
-5:39
-then this is the full solver file, add what not there already:
-##solver.rb
-class Solver
-  def factorial(number)
-    raise ArgumentError, 'Input must be a non-negative integer' if number.negative?
-    (1..number).reduce(1, :*)
-  end
-  def reverse(word)
-    word.reverse
-  end
-  def fizzbuzz(input_number)
-    return 'fizzbuzz' if (input_number % 3).zero? && (input_number % 5).zero?
-    return 'fizz' if (input_number % 3).zero?
-    return 'buzz' if (input_number % 5).zero?
-    input_number.to_s
-  end
-end
-console = Solver.new
-puts console.factorial(5)
-puts console.reverse('hello')
-puts console.fizzbuzz(5)
